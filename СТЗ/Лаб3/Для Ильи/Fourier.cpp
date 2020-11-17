5e8a10e4f539e91c55bdab63c28936d91e8170aa
Aleksey Samarin <54943504+Uiopio@users.noreply.github.com>
2020-11-17 14:20:27 +0300

    Update Fourier.cpp

--- СТЗ/Лаб3/Для Ильи/Fourier.cpp
+++ СТЗ/Лаб3/Для Ильи/Fourier.cpp
@@ -1,6 +1,3 @@
-#include "Fourier.h"
-
-
 int Fourier::corr2(Mat inputImage1, Mat inputImage2)
 {
 	int mean1 = 0;
@@ -14,7 +11,7 @@ int Fourier::corr2(Mat inputImage1, Mat inputImage2)
 	mean1 = mean1 / (inputImage1.rows * inputImage1.cols);
 	cout << "среднее значение для картинки 1: " << mean1 << endl;
 	Mat image1Mean = inputImage1 - mean1;
-
+	imshow("image1Mean", image1Mean);
 
 	int mean2 = 0;
 	for (int i = 0; i < inputImage2.rows; i++)
@@ -27,6 +24,18 @@ int Fourier::corr2(Mat inputImage1, Mat inputImage2)
 	mean2 = mean2 / (inputImage2.rows * inputImage2.cols);
 	cout << "среднее значение для картинки 2: " << mean2 << endl;
 	Mat image2Mean = inputImage2 - mean2;
+	imshow("image2Mean", image2Mean);
+
+	Mat Mean1;
+	Mat Std1;
+	meanStdDev(image1Mean, Mean1, Std1);
+
+	Mat Mean2;
+	Mat Std2;
+	meanStdDev(image2Mean, Mean2, Std2);
+
+	image1Mean = inputImage1 / Std1;
+	image2Mean = inputImage2 / Std2;
 
 
 
@@ -47,8 +56,13 @@ int Fourier::corr2(Mat inputImage1, Mat inputImage2)
 	cv::dft(Glaza, GlazaForward, DFT_COMPLEX_OUTPUT);
 
 	Mat GF;
-	//mulSpectrums(FurForward, GlazaForward, GF, 0, 1);
-	mulSpectrums(GlazaForward, FurForward, GF, 0, 1);
+	mulSpectrums(FurForward, GlazaForward, GF, 0, 1);
+	//mulSpectrums(GlazaForward, FurForward, GF, 0, 1);
+
+	
+
+
+
 
 	Mat rezultInverse(GF.size(), CV_32FC1, Scalar(0));
 	cv::dft(GF, rezultInverse, DFT_INVERSE | DFT_REAL_OUTPUT);
