2a47f3e3f922f3763132b63119141bc1369a89d8
Aleksey Samarin <54943504+Uiopio@users.noreply.github.com>
2021-05-25 15:19:56 +0300

    Add files via upload

--- Работа/GeoPioneerTest/Client.py
+++ Работа/GeoPioneerTest/Client.py
@@ -1,8 +1,11 @@
 import socket
 import time
-from pioneer import Pioneer
+import cv2
+import numpy as np
+import math
 
-host = "10.10.16.173"
+
+host = "10.10.16.179"
 port = 8000
 
 class Client():
@@ -14,12 +17,11 @@ class Client():
             self.sock.connect((server_ip, server_port))
 
             # Отправляем стартовое сообщение
-            output_message = "start"
-            self.sock.sendall(bytes(output_message, encoding="utf-8"))
+            self.send_message("start")
 
             # Проверяем ответ от сервера
-            input_message = self.sock.recv(1024)
-            if input_message.decode("utf-8") == "successful":
+            answer_server = self.accept_message()
+            if answer_server == "successful":
                 print("Успешное подключение")
 
         except:
@@ -33,14 +35,108 @@ class Client():
         print("Отключение от сервера")
         self.sock.close()
 
+    def accept_message(self):
+        message = self.sock.recv(1024)
+        return message.decode("utf-8")
+
     def send_message(self, output_message):
         self.sock.sendall(bytes(output_message, encoding="utf-8"))
 
+class Message():
+    arm = "arm"
+    disarm = "disarm"
+
+    def led(self, r,g,b):
+        message = "L:" + " R" + str(r) + " G" + str(g) +" B" + str(b)
+        return message
+
+    def motion(self, x,y,z,Y):
+        message = "M:" + " X" + str(x) + " Y" + str(y) + " Z" + str(z) + " A" + str(Y)
+        return message
+
 
 if __name__ == '__main__':
+    mes = Message()
     client = Client(host, port)
 
-    client.send_message("arm")
+    client.send_message(mes.led(125, 20, 0))
 
-    time.sleep(3)
-    client.send_message("disarm")
+    time.sleep(5)
+
+    client.send_message(mes.led(0, 0, 0))
+
+    window = np.zeros((300, 400, 3), np.uint8)
+
+    command_x = float(0)
+    command_y = float(0)
+    command_z = float(1)
+    command_yaw = math.radians(float(0))
+    increment_xy = float(0.2)
+    increment_z = float(0.1)
+    increment_deg = math.radians(float(90))
+
+    new_command = False
+
+    while(True):
+        cv2.imshow('window', window)
+        key = cv2.waitKey(1)
+        if key == 27:  # esc
+            print('esc pressed')
+            cv2.destroyAllWindows()
+            client.send_message("land")
+            client.disconnect()
+            break
+
+        elif key == ord('w'):
+            print('w')
+            command_y += increment_xy
+            new_command = True
+
+        elif key == ord('s'):
+            print('s')
+            command_y -= increment_xy
+            new_command = True
+
+        elif key == ord('a'):
+            print('a')
+            command_x -= increment_xy
+            new_command = True
+
+        elif key == ord('d'):
+            print('d')
+            command_x += increment_xy
+            new_command = True
+
+        elif key == ord('q'):
+            print('q')
+            client.send_message("arm")
+            new_command = True
+
+        elif key == ord('e'):
+            print('e')
+            client.send_message("disarm")
+            new_command = True
+
+        elif key == ord('r'):
+            print('r')
+            client.send_message("takeoff")
+            new_command = True
+
+        elif key == ord('h'):
+            print('h')
+            command_z += increment_z
+            new_command = True
+
+        elif key == ord('l'):
+            print('l')
+            command_z -= increment_z
+            new_command = True
+
+        if new_command:
+            client.send_message(mes.motion(command_x, command_y, command_z, command_yaw))
+            new_command = False
+
+
+
+
+
--- Работа/GeoPioneerTest/Server.py
+++ Работа/GeoPioneerTest/Server.py
@@ -1,9 +1,11 @@
 import socket
 import time
+import os
+#import cv2
 
 from pioneer import Pioneer
 
-host = "10.10.16.173"
+host = "10.10.16.179"
 port = 8000
 
 class Server():
@@ -21,8 +23,7 @@ class Server():
         print("Connected by", self.addr)
 
         # Отправляем клтенту подтверждение подключения
-        output_message = "successful"
-        self.conn.sendall(bytes(output_message, encoding="utf-8"))
+        self.send_message("successful")
 
     # Принять сообщение
     def accept_message(self):
@@ -34,16 +35,68 @@ class Server():
         self.conn.sendall(bytes(output_message, encoding="utf-8"))
 
 
+
+def message_perse(message):
+    # Если сообщение начинается с буквы L (led_control) то возврашаем 3 значения интенсивности
+    # Пример сообщения: L: R1 G20 B255
+    if message[0] == "L":
+        # Ищем индексы опорных букв
+        ind_R = message.find("R")
+        ind_G = message.find("G")
+        ind_B = message.find("B")
+
+        R = float(message[(ind_R + 1):(ind_G - 1)])
+        G = float(message[(ind_G + 1):(ind_B - 1)])
+        B = float(message[(ind_B + 1):(len(message))])
+
+        return R, G, B
+
+    # Если сообщение начинается с буквы M (motion) то возврашаем 4 значения координат (X, Y, Z, Angle)
+    elif message[0] == "M":
+        ind_X = message.find("X")
+        ind_Y = message.find("Y")
+        ind_Z = message.find("Z")
+        ind_A = message.find("A")
+
+        X = float(message[(ind_X + 1):(ind_Y - 1)])
+        Y = float(message[(ind_Y + 1):(ind_Z - 1)])
+        Z = float(message[(ind_Z + 1):(ind_A - 1)])
+        A = float(message[(ind_A + 1):(len(message))])
+
+        return X, Y, Z, A
+
+
 if __name__ == '__main__':
     pioneer = Pioneer(logger=True)
 
     server = Server(host, port)
 
     while(True):
-        message = server.accept_message()
+
+        message = str(server.accept_message())
 
         if(message == "arm"):
             pioneer.arm()
+
         elif (message == "disarm"):
             pioneer.disarm()
+
+        elif (message == "takeoff"):
+            pioneer.takeoff()
+
+        elif (message == "land"):
+            pioneer.land()
+
+        elif (message.find("L") != -1) or (message.find("M") != -1):
+            if message[0] == "L":
+                print(message)
+                R, G, B = message_perse(message)
+                pioneer.led_control(r=R, g=G, b=B)
+
+            elif message[0] == "M":
+                print(message)
+                X, Y, Z, Angle = message_perse(message)
+                pioneer.go_to_local_point( x=X, y=Y, z=Z, yaw=Angle)
+
+
 
